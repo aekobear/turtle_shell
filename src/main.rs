b4958e12789d75efbde9eca0b772b097877f22ce
eiko <kokumeiko@gmail.com>
2017-04-04 12:14:02 -0400

    Implement parser in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(box_syntax, box_patterns)]
+
 use std::io;
 
 extern crate plugin;
@@ -21,4 +23,48 @@ fn main() {
     if let Value::Text(response) = w.send(Message::new("get_wallpaper".to_string())) {
         println!("get_wallpaper: {}", response);
     }
+}
+
+
+enum Command {
+    Literal(String),
+    Expression { params: Vec<Command> },
+}
+
+fn parse_command(text: &str) -> Command {
+    _parse_command(&mut text.chars())
+}
+
+fn _parse_command(chars: &mut std::str::Chars) -> Command {
+
+    let mut params = vec![];
+
+    let mut word = String::new();
+
+    while let Some(c) = chars.next() {
+        if c.is_whitespace() {
+            if !word.is_empty() {
+                params.push(Command::Literal(word));
+            }
+            word = String::new();
+        } else if c == '(' {
+            if !word.is_empty() {
+                params.push(Command::Literal(word));
+            }
+            word = String::new();
+            params.push(_parse_command(chars));
+        } else if c == ')' {
+            if !word.is_empty() {
+                params.push(Command::Literal(word));
+            }
+            return Command::Expression { params: params };
+        } else {
+            word.push(c);
+        }
+    }
+
+    if !word.is_empty() {
+        params.push(Command::Literal(word));
+    }
+    Command::Expression { params: params }
 }
